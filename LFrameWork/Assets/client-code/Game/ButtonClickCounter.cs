871d4dfb661cc26aa1d88b9e8cb6e8e9e81e3590
silai.zhou <silai.zhou@puzzleplusgames.com>
2021-06-09 16:53:37 +0800

    不规则点击

---
+++ LFrameWork/Assets/client-code/Game/ButtonClickCounter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonClickCounter : MonoBehaviour
+{
+    public Button btn;
+    int count = 0;
+
+    void Start()
+    {
+        btn.onClick.AddListener(() =>
+        {
+            Click();
+        });
+    }
+
+
+    public void Click()
+    {
+        count++;
+        Debug.LogError(count);
+        //Debug.Log(string.Format("{0}点击了{1}次！", btnName, count));
+    }
+}
